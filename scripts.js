75076fd31e550d0541dce7474338db8b02bcb7c6
throstur78 <42745670+throstur78@users.noreply.github.com>
2018-11-11 22:20:04 +0000

    Vann i synidæmi

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://apis.is/car?number=';
+const API_URL = 'https://apis.is/car?number=';
 
 document.addEventListener('DOMContentLoaded', function () {
   const cars = document.querySelector('.cars');
@@ -64,7 +64,7 @@ const program = (() => {
       .catch((error) => {
         displayError('Villa!');
         console.error(error);
-      })
+      });
   }
 
   function onSubmit(e) {
